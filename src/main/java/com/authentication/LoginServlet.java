662084fc22b5be3b1928251a1c0e7ccf521d0f00
J <dafunnyj@gmail.com>
2018-05-07 16:56:13 +0100

    Initial commit

---
+++ src/main/java/com/authentication/LoginServlet.java
@@ -0,0 +1,57 @@
+package com.authentication;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	String validName="John";
+	String validPassword="password";
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String name=request.getParameter("username");
+		String password=request.getParameter("password");
+		
+		if(validName.equals(name)&&validPassword.equals(password)) {
+			Cookie loginCookie =new Cookie("name",name);
+			loginCookie.setMaxAge(600);
+			
+			response.addCookie(loginCookie);
+			
+			//Foreward on to new index page
+			response.sendRedirect("/welcome.jsp");
+		}
+		
+		
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/com/test/Hello.java
@@ -0,0 +1,46 @@
+package com.test;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet("/HelloWorld")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Hello() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ServletContext sc=getServletConfig().getServletContext();
+		RequestDispatcher rd =sc.getNamedDispatcher("GoodbyeWorld");
+		rd.forward(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append("By the way HelloWolrd");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
